94ea1e83f58a38c7263fd46f4ba2ed65e7cc8156
chomyeong <383390930@qq.com>
2020-04-06 13:52:27 +0800

    repair setData not define

--- README.md
+++ README.md
@@ -1,45 +1,109 @@
-# :rocket::star2:PHP API通用错误码
+# :rocket:PHP API通用错误码
 
-## Feature
+## errorcode
 
- - 无需手动定义懵逼的错误码数字
+在写接口API返回错误码时，我们以前可能这样做：
+
+```php
+$ret = [
+    'code'=>100,
+    'msg'=>'message',
+    'data'=>[],
+];
+
+echo json_encode($ret);
+```
+
+写着写着，你可能经常忘记了错误码从哪里开始了...  🤦🏼‍♂️
+
+**用了这个扩展后，我们可以这样：**
+
+```php
+return [
+    ['errDefine' => 'ERROR_SHOP_NAME', 'index' => 1000, 'errMsg' => '商城名称错误'],
+    ['errDefine' => 'ERROR_SHOP_NAME1','errMsg' => '商城名称错误1'],
+    ['errDefine' => 'ERROR_ORDER_ERROR', 'index' => 2000, 'errMsg' => '商城登录错误'],
+];
+```
+
+比如错误码1000到1999是商城错误，定义好1000,第二个错误无需定义错误码，即可自增 👏
+
+## ✨Feature
+
+ -  无需手动定义懵逼的错误码数字
  - 定义好错误码初始值即可自动递增
  - 不用在纠结错误码放在哪
 
-## Requirement
+## 🖥Requirement
 
-1. PHP >= 5.3
+1. PHP >= 5.4
 2. **[composer](https://getcomposer.org/)**
 
-> 对所使用的框架并无特别要求
-
-## Installation
+## 📦Installation
 
 ```shell
 composer require "chomyeong/errorcode" "v1.0.0"
 ```
 
-## Usage
+## 🔨Usage
 
 基本使用:
 
 在vendor同级目录新建errors目录，新增code.php，格式如下：
 
 ```php
-return array(
-    array('errDefine' => 'ERROR_SHOP_XXX', 'index' => 1000, 'errMsg' => '哦也'),
-    // 错误码 
-    array('errDefine' => 'ERROR_SHOP_XXX', 'index' => 2000, 'errMsg' => '哦也'),
-);
+return [
+    ['errDefine' => 'ERROR_SHOP_NAME', 'index' => 1000, 'errMsg' => '商城名称错误'],
+    ['errDefine' => 'ERROR_SHOP_LOGIN', 'index' => 2000, 'errMsg' => '商城登录错误'],
+];
 ```
 
-调用：
+引入扩展包：
 
 ```php
 use chomyeong\errorcode\Error
 $err = new Error();
-$err->responseError(ERROR_PARAM); // 相应结果 参数错误
-// {"err":1,"errMsg":"\u53c2\u6570\u9519\u8bef\uff01","data":{},"isError":true}
+```
+
+响应错误：
+
+```php
+$err->responseError(ERROR_PARAM);
+```
+
+返回格式：
+
+![image-20200406130217004](/Users/zhaoming/Library/Application Support/typora-user-images/image-20200406130217004.png)
+
+响应成功：
+
+```php
+// 要返回的数据
+$data = [
+    'info' => [
+        'id'   => 1,
+        'username' => 'chomyeong',
+    ],
+];
+$err->responseSuccess($data);
+```
+
+返回格式：
+
+![image-20200406125922294](/Users/zhaoming/Library/Application Support/typora-user-images/image-20200406125922294.png)
+
+默认错误码：
+
+```
+ERROR_PARAM 参数错误
+ERROR_SYSTEM 系统错误
+ERROR_SESSION_ERROR 会话不存在
+ERROR_SESSION_PRIVILEGE_ERROR 权限不正确
+ERROR_MODIFY_INFO_FAILED 修改数据失败
+ERROR_ACCOUNT_LOGIN_FAILED 账号名或密码错误
+ERROR_ACCOUNT_RELOGIN_FAILED_WRONG_TOKEN 重登录失败：令牌错误
+ERROR_ACCOUNT_RELOGIN_FAILED_TOKEN_TIMEOUT 重登录失败：令牌已超时
+ERROR_ACCOUNT_PASSPORT_EXISTS 账号已存在
 ```
 
 ## License
--- src/Error.php
+++ src/Error.php
@@ -11,7 +11,7 @@
 
 /**
  * Error.php.
- *
+ * 通用API 错误码
  * @author    chomyeong <mme7wan@gmail.com>
  * @copyright 2020 chomyeong <mme7wan@gmail.com>
  *
@@ -26,36 +26,25 @@ class Error
 
     private $errCode = 0;
     private $errMsg = "";
-    private $retData = array();
+    private $retData = [];
     private $err2Msg = [];
 
-    static $errors = array(
-        array('errDefine' => 'ERROR_OK', 'index' => 0, 'errMsg' => ''),
+    static $errors = [
+        ['errDefine' => 'ERROR_OK', 'index' => 0, 'errMsg' => ''],
 
         // 通用错误
-        array('errDefine' => 'ERROR_PARAM', 'index' => 1, 'errMsg' => '参数错误！'),
-        array('errDefine' => 'ERROR_SYSTEM', 'errMsg' => '系统错误！'),
-        array('errDefine' => 'ERROR_SESSION_ERROR', 'errMsg' => '会话不存在！'),
-        array('errDefine' => 'ERROR_SESSION_PRIVILEGE_ERROR', 'errMsg' => '权限不正确！'),
-        array('errDefine' => 'ERROR_MODIFY_INFO_FAILED', 'errMsg' => '修改数据失败！'),
+        ['errDefine' => 'ERROR_PARAM', 'index' => 1, 'errMsg' => '参数错误！'],
+        ['errDefine' => 'ERROR_SYSTEM', 'errMsg' => '系统错误！'],
+        ['errDefine' => 'ERROR_SESSION_ERROR', 'errMsg' => '会话不存在！'],
+        ['errDefine' => 'ERROR_SESSION_PRIVILEGE_ERROR', 'errMsg' => '权限不正确！'],
+        ['errDefine' => 'ERROR_MODIFY_INFO_FAILED', 'errMsg' => '修改数据失败！'],
 
         // 账号相关错误
-        array('errDefine' => 'ERROR_ACCOUNT_LOGIN_FAILED', 'index' => 101, 'errMsg' => '账号名或密码错误！'),
-        array('errDefine' => 'ERROR_ACCOUNT_RELOGIN_FAILED_WRONG_TOKEN', 'errMsg' => '重登录失败：令牌错误!'),
-        array('errDefine' => 'ERROR_ACCOUNT_RELOGIN_FAILED_TOKEN_TIMEOUT', 'errMsg' => '重登录失败：令牌已超时!'),
-        array('errDefine' => 'ERROR_ACCOUNT_PASSPORT_EXISTS', 'errMsg' => '该账号已存在！'),
-        array('errDefine' => 'ERROR_ACCOUNT_NOT_EXIST_PASSPORT', 'errMsg' => '账号不存在！'),
-        array('errDefine' => 'ERROR_ACCOUNT_RELOGIN_FAILED', 'errMsg' => '设置重登录信息出错!'),
-        array('errDefine' => 'ERROR_ACCOUNT_WRONG_PLATFORM', 'errMsg' => '未知的用户平台!'),
-        array('errDefine' => 'ERROR_ACCOUNT_NO_INVITE_CODE', 'errMsg' => '您不能填写自己的邀请码!'),
-        array('errDefine' => 'ERROR_ACCOUNT_NO_INVITE', 'errMsg' => '您填写的邀请码不存在!'),
-        array('errDefine' => 'ERROR_ACCOUNT_PERMANENT_TITLE', 'errMsg' => '此账号已被永久封号！'),
-        array('errDefine' => 'ERROR_ACCOUNT_UPDATE_PASSWORD_FAIL', 'errMsg' => '修改密码失败,请稍后再试！'),
-        array('errDefine' => 'ERROR_ACCOUNT_FORBIDDEN', 'errMsg' => '账号已被封禁，如有疑虑请您联系客服!'),
-        array('errDefine' => 'ERROR_ACCOUNT_ADMIN_EXISTS', 'errMsg' => '管理员已存在!'),
-        array('errDefine' => 'ERROR_ACCOUNT_USER_EXISTS', 'errMsg' => '用户已存在!'),
-        array('errDefine' => 'ERROR_ACCOUNT_ADMIN_CANT_DELETE_SELF', 'errMsg' => '不可以删除自己!'),
-    );
+        ['errDefine' => 'ERROR_ACCOUNT_LOGIN_FAILED', 'index' => 101, 'errMsg' => '账号名或密码错误！'],
+        ['errDefine' => 'ERROR_ACCOUNT_RELOGIN_FAILED_WRONG_TOKEN', 'errMsg' => '重登录失败：令牌错误!'],
+        ['errDefine' => 'ERROR_ACCOUNT_RELOGIN_FAILED_TOKEN_TIMEOUT', 'errMsg' => '重登录失败：令牌已超时!'],
+        ['errDefine' => 'ERROR_ACCOUNT_PASSPORT_EXISTS', 'errMsg' => '账号已存在！'],
+    ];
 
     public function __construct()
     {
@@ -101,10 +90,10 @@ class Error
      * 设置错误码，在其中会同时设置好错误信息
      * @param $errCode  错误码
      */
-    function setErr($errCode)
+    private function setErr($errCode)
     {
         if ($this->errCode != ERROR_OK) {
-            // $this->log->write_log('notice', 'Error code is set, are you sure of setting error code again???');
+            // Error code is set, are you sure of setting error code again???
         }
 
         $this->errCode = $errCode;
@@ -112,10 +101,19 @@ class Error
     }
 
     /**
+     * 设置返回消息体
+     * @param $retData 消息体
+     */
+    private function setRetData($retData)
+    {
+        $this->retData = $retData;
+    }
+
+    /**
      * 获取当前的错误码
      * @return int  当前错误码
      */
-    public function getErrCode()
+    private function getErrCode()
     {
         return $this->errCode;
     }
@@ -124,7 +122,7 @@ class Error
      * 获取当前的错误描述
      * @return string   当前错误描述
      */
-    public function getErrMsg()
+    private function getErrMsg()
     {
         return $this->errMsg;
     }
@@ -132,16 +130,15 @@ class Error
     /**
      * 加载默认的View，其中只是返回json串
      */
-    public function loadDefaultView($isError = false)
+    private function loadDefaultView($isError = false)
     {
 
-        $data = array
-        (
+        $data = [
             'err'     => $this->errCode,
             'errMsg'  => $this->errMsg,
             'data'    => (object) $this->retData,
             'isError' => $isError,
-        );
+        ];
 
         echo json_encode($data);
     }
@@ -163,7 +160,7 @@ class Error
     public function responseSuccess($data = null)
     {
         if ($data == null) {
-            $data = (object) array();
+            $data = (object) [];
         }
         $this->setRetData($data);
         $this->loadDefaultView();
@@ -188,11 +185,10 @@ class Error
         self::$errors = array_merge(self::$errors, $errors);
     }
 
-
     /**
      * 初始化 查找用户自定义错误文件
      */
-    public function init()
+    private function init()
     {
         require_once 'Exceptions/Exception.php';
         $rootDir = str_replace('\\', '/', realpath(dirname(dirname(dirname(dirname(__DIR__)).'/'))))."/";
@@ -203,12 +199,12 @@ class Error
                 $arr = require_once $target.'/'.'code.php';
                 self::registerErrors($arr);
             } else {
-                // 抛出异常
+                // 错误文件不存在
                 throw new \Exception("code.php not exist!");
             }
 
         } else {
-            // 抛出异常
+            // 错误文件不存在
             throw new \Exception("code.php not exist!");
         }
 
@@ -220,7 +216,7 @@ class Error
      * @param $errCode  错误码
      * @return string   错误描述
      */
-    public function getErrMsginfo($errCode)
+    private function getErrMsginfo($errCode)
     {
         if (array_key_exists($errCode, $this->err2Msg)) {
             return $this->err2Msg[$errCode];
